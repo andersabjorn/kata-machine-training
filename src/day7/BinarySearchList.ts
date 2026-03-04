b55a04d1b00779ff2014565cedae6eb592f6c1f4
Anders Abjörn <anteabjorn@gmail.com>
2026-03-04 19:30:03 +0100

    Faster inaryserach today

--- src/day7/BinarySearchList.ts
+++ src/day7/BinarySearchList.ts
@@ -1,10 +1,11 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
     let hi = haystack.length;
+    
     do {
-        const m = Math.floor(lo + (hi - lo) / 2);
+        const m = Math.floor(lo +(hi - lo) / 2);
         const v = haystack[m];
-
+        
         if (v === needle) {
             return true;
         } else if (v > needle) {
@@ -12,8 +13,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
         } else {
             lo = m + 1;
         }
-
     } while (lo < hi);
-    return false;
 
+            return false;
 }
