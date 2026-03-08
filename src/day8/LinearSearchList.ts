c0946244b1d79f4b0c468151255ebba508624d6e
Anders Abjörn <anteabjorn@gmail.com>
2026-03-08 11:29:29 +0100

    LinearSearch is ineffective

--- src/day8/LinearSearchList.ts
+++ src/day8/LinearSearchList.ts
@@ -1,8 +1,13 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
-    for (let i = 0; haystack.length; i++) {
+    
+    // Här går vi igenom varje steg i listan för att leta efter nålen
+    for (let i = 0; i < haystack.length; i++) {
+        // här sätter vi upp regler för vad vi ska leta efter
         if (haystack [i] === needle) {
+            // Hittar vi värdet så returnerer vi sant
             return true;
         }
     }
+    // Om annat falskt, alltså har vi 1,5,9,10 i arrayen och vi söker efter 18 så kommer vi få ett falskt värde
     return false;
 }
